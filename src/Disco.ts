e68f2d5393223e037ed49b4b1a75d947c1e384ef
RomiTorres <torres_rominav@hotmail.com>
2025-09-24 20:12:04 +0200

    Clase Disco

---
+++ src/Disco.ts
@@ -0,0 +1,44 @@
+export class Disco {
+  #nombre: string;
+  #capacidad: number;
+  #contenido: string;
+  #tipo: string;
+
+  constructor(nuevoNombre: string, capacidad: number, tipo: string) {
+    this.#nombre = nuevoNombre;
+    this.#capacidad = capacidad;
+    this.#tipo = tipo;
+  }
+
+  get nombre(): string {
+    return this.#nombre;
+  }
+
+  set nombre(nuevoNombre: string) {
+    this.#nombre = nuevoNombre;
+  }
+
+  get capacidad(): number {
+    return this.#capacidad;
+  }
+
+  set capacidad(capacidad: number) {
+    this.#capacidad = capacidad;
+  }
+
+  get contenido(): string {
+    return this.#contenido;
+  }
+
+  set contenido(contenido: string) {
+    this.#contenido = contenido;
+  }
+
+  get tipo(): string {
+    return this.#tipo;
+  }
+
+  set tipo(tipo: string) {
+    this.#tipo;
+  }
+}
--- src/DiscoDuro.ts
+++ src/DiscoDuro.ts
@@ -1,3 +1,8 @@
 export class DiscoDuro {
-  
+  #nombre: string;
+  #capacidad: number;
+  #contenido: string;
+  #tipo: string;
+
+  constructor(nombre: string, capacidad: number, tipo: string) {}
 }
