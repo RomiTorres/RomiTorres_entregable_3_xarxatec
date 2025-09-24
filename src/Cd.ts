11c142e024b010e89c4326d88a472813368c7e88
RomiTorres <torres_rominav@hotmail.com>
2025-09-24 20:30:38 +0200

    Clase CD

--- src/Cd.ts
+++ src/Cd.ts
@@ -1,3 +1,31 @@
-export class Cd {
-  
+import { Disco } from "./Disco.js";
+import { IDiscoGiratorio } from "./IDiscoGiratorio.js";
+
+export class Cd extends Disco implements IDiscoGiratorio {
+  constructor(nuevoNombre: string, capacidad: number, tipo: string) {
+    super(nuevoNombre, capacidad, tipo);
+  }
+
+  girarDisco(): void {
+    console.log("CD girando a 200 RPM");
+  }
+
+  almacenarDatos(datos: string): void {
+    this.contenido += datos;
+    console.log("Datos almacenados en CD");
+  }
+
+  leerDatos(): void {
+    console.log(`Leyendo datos del CD con láser: ${this.contenido}`);
+  }
+
+  escribirDatos(datos: string): void {
+    console.log(`Escribiendo en CD con láser: ${datos}`);
+  }
+
+  mostrarInformacion(): void {
+    console.log(
+      `Nombre: ${this.nombre}, Capacidad: ${this.capacidad}Mb, Tipo: ${this.tipo}, Contenido: ${this.contenido}`
+    );
+  }
 }
