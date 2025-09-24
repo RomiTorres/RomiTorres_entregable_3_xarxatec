db70566a733358837d5029041a5ef89c429a492b
RomiTorres <torres_rominav@hotmail.com>
2025-09-24 21:07:57 +0200

    Confirmacion en index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hola mundo!")
+import { Cd } from "./Cd.js";
+import { DiscoDuro } from "./DiscoDuro.js";
+
+const cdRock = new Cd('CD de música', 700, 'CD');
+const discoDuroExterno = new DiscoDuro('Disco externo', 1000, 'SSD');
+
+cdRock.girarDisco();
+cdRock.escribirDatos("Rolling Stones");
+cdRock.leerDatos();
+cdRock.mostrarInformacion();
+
+discoDuroExterno.girarDisco();
+discoDuroExterno.escribirDatos('Contrato laboral');
+discoDuroExterno.leerDatos();
+discoDuroExterno.mostrarInformacion();
