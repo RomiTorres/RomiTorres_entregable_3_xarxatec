d76ed023cc94060d2420603efbc717025ccd148c
RomiTorres <torres_rominav@hotmail.com>
2025-09-24 20:48:42 +0200

    Clase Disco duro

--- src/DiscoDuro.ts
+++ src/DiscoDuro.ts
@@ -1,8 +1,32 @@
-export class DiscoDuro {
-  #nombre: string;
-  #capacidad: number;
-  #contenido: string;
-  #tipo: string;
+import { Disco } from "./Disco.js";
+import { IDiscoGiratorio } from "./IDiscoGiratorio.js";
 
-  constructor(nombre: string, capacidad: number, tipo: string) {}
+export class DiscoDuro extends Disco implements IDiscoGiratorio {
+  constructor(nuevoNombre: string, capacidad: number, tipo: string) {
+    super(nuevoNombre, capacidad, tipo);
+  }
+
+  girarDisco(): void {
+    console.log("Disco duro girando a 8000 RPM");
+  }
+
+  almacenarDatos(datos: string): void {
+    this.contenido;
+  }
+
+  leerDatos(): void {
+    console.log(
+      `Leyendo datos del disco duro con cabezal magnético: ${this.contenido}`
+    );
+  }
+
+  escribirDatos(datos: string): void {
+    console.log(`Escribiendo en Disco Duro: ${datos}`);
+  }
+
+  mostrarInformacion(): void {
+    console.log(
+      `Nombre: ${this.nombre}, Capacidad: ${this.capacidad}Mb, Tipo: ${this.tipo}, Contenido: ${this.contenido}`
+    );
+  }
 }
