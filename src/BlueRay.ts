257a9d0b4bd9340e6226001229b3003d1c272e1a
RomiTorres <torres_rominav@hotmail.com>
2025-09-25 08:00:07 +0200

    Bonus - BlueRay, clase sin probar

---
+++ src/BlueRay.ts
@@ -0,0 +1,31 @@
+import { Disco } from "./Disco.js";
+import { IDiscoGiratorio } from "./IDiscoGiratorio.js";
+
+export class BlueRay extends Disco implements IDiscoGiratorio {
+  constructor(nuevoNombre: string, capacidad: number, tipo: string) {
+    super(nuevoNombre, capacidad, tipo);
+  }
+
+  girarDisco(): void {
+    console.log("BlueRay girando a 1000 RPM");
+  }
+
+  almacenarDatos(datos: string): void {
+    this.contenido += datos;
+    console.log("Datos almacenados en BlueRay");
+  }
+
+  leerDatos(): void {
+    console.log(`Leyendo datos del CD con láser: ${this.contenido}`);
+  }
+
+  escribirDatos(datos: string): void {
+    console.log(`Escribiendo en BlueRay: ${datos}`);
+  }
+
+   mostrarInformacion(): void {
+    console.log(
+      `Nombre: ${this.nombre}, Capacidad: ${this.capacidad}Mb, Tipo: ${this.tipo}, Contenido: ${this.contenido}`
+    );
+  }
+}
